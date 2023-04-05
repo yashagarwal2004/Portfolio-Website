f772c8a59f0c49760acf7e81b65edcb32656c316
yashagarwal2004 <114741259+yashagarwal2004@users.noreply.github.com>
2023-04-06 01:04:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed("#description",{
+    strings: ['Student', 'Web Developer', 'Competitive Coder'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop:true,
+});
